817c175c1e46f70ff49f23f7de090fad4818649c
bizhermit <rsenda@bizhermit.com>
2025-07-15 19:03:55 +0900

    feat: playwright

--- .devcontainer/initialize-playwright.sh
+++ .devcontainer/initialize-playwright.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y --no-install-recommends \
-  fonts-noto-cjk \
-  fonts-mplus \
-  fonts-ipafont-gothic \
-  fonts-ipafont-mincho
-
-apt-get clean
-
-rm -rf /var/lib/apt/lists/*
-
 echo "node" | sudo -S npx playwright install-deps
 
 npx playwright install
--- .devcontainer/postCreatedCommand.sh
+++ .devcontainer/postCreatedCommand.sh
@@ -2,4 +2,4 @@
 
 echo "node" | sudo -S chown node /workspace/node_modules && sudo -S chmod 777 /workspace/node_modules
 echo "node" | sudo -S chown node /workspace/.react-router && sudo -S chmod 777 /workspace/.react-router
-echo "node" | sudo -S chown node /workspace/build && sudo -S chmod 777 /workspace/build
+echo "node" | sudo -S chown node /workspace/.playwright && sudo -S chmod 777 /workspace/.playwright
