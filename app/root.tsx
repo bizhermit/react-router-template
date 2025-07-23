e2798f994c99203de377ace0ef28ed05a33524cc
bizhermit <rsenda@bizhermit.com>
2025-07-23 19:12:30 +0900

    fix: interactive-widget resizes-content

--- app/root.tsx
+++ app/root.tsx
@@ -39,7 +39,7 @@ export function Layout({ children }: { children: ReactNode; }) {
       >
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, interactive-widget=resizes-content" />
           <Meta />
           <Links />
         </head>
