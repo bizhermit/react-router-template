d1e4e5a6b887b59de89261945f7c29d713820d69
bizhermit <rsenda@bizhermit.com>
2025-08-18 21:00:16 +0900

    refact

--- app/components/auth/server/fetch-auth.ts
+++ app/components/auth/server/fetch-auth.ts
@@ -8,30 +8,12 @@ interface FetchAuthParams {
   method?: "GET" | "POST";
 };
 
-const isDev = process.env.NODE_ENV === "development";
-const selfOrigin = process.env.AUTH_URL || `http://localhost:${isDev ? (process.env.DEV_PORT || 5173) : (process.env.PORT || 3000)}`;
-
 export async function fetchAuth<T>({
   request,
   action,
   method,
 }: FetchAuthParams): Promise<T | null> {
   try {
-    // const url = new URL(`/auth/${action}`, selfOrigin).toString();
-    // const cookie = request.headers.get("cookie") || "";
-    // // const res = await fetch(url, {
-    // //   method: method || "GET",
-    // //   headers: { cookie },
-    // // });
-    // console.log(cookie);
-    // const req = new Request(url, {
-    //   method: method || "GET",
-    //   headers: { cookie },
-    // });
-    // const res = await (method === "POST" ? authAction : authLoader)(req);
-    // if (!res.ok) return null;
-    // return res.json().catch(() => null) as T | null;
-    // 元のリクエストのURLを/auth/{action}に変更
     const url = new URL(request.url);
     url.pathname = `/auth/${action}`;
 
@@ -42,7 +24,6 @@ export async function fetchAuth<T>({
     });
 
     const res = await (method === "POST" ? authAction : authLoader)(req);
-    console.log(res);
     if (!res.ok) return null;
     return res.json().catch(() => null) as T | null;
   } catch {
@@ -55,12 +36,5 @@ export async function authLoader(request: Request) {
 };
 
 export async function authAction(request: Request) {
-  console.warn("//////////////////// action", {
-    url: request.url,
-    method: request.method,
-    pathname: new URL(request.url).pathname,
-    hasBody: request.body !== null,
-    contentType: request.headers.get("content-type"),
-  });
   return Auth(request, authConfig);
 };
