4608d1bfcf04326f11e871a026ef8b0274a4b33e
bizhermit <rsenda@bizhermit.com>
2025-08-21 01:27:38 +0900

    fix: textarea fit content height supported wrap

--- app/components/react/elements/form/text-area.tsx
+++ app/components/react/elements/form/text-area.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, type ChangeEvent } from "react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import { useSchemaItem } from "~/components/react/hooks/schema";
 import { getValidationValue, InputField, type InputWrapProps } from "./common";
 import type { FormItemHookProps } from "./hooks";
@@ -61,6 +61,7 @@ export function TextArea<D extends Schema.DataItem<Schema.$String>>({
       if (!ref.current) return;
       const sv = String(value || "");
       if (ref.current.value !== sv) ref.current.value = sv;
+      calcFitContentHeight();
     },
     effectContext: function () {
       setMinLen(getMinLen);
@@ -83,9 +84,36 @@ export function TextArea<D extends Schema.DataItem<Schema.$String>>({
 
   const [maxLen, setMaxLen] = useState<number | undefined>(getMaxLen);
 
+  function calcFitContentHeight() {
+    if (propsRows !== "fit" || !ref.current) return;
+
+    ref.current.style.height = "0";
+    const height = ref.current.scrollHeight;
+
+    if (maxRows == null) {
+      ref.current.style.height = height + "px";
+      ref.current.style.overflowY = "hidden";
+      return;
+    }
+
+    const marginBlock = parseFloat(getComputedStyle(ref.current).paddingTop) +
+      parseFloat(getComputedStyle(ref.current).paddingBottom);
+    const lineHeight = parseFloat(getComputedStyle(ref.current).lineHeight);
+    const maxHeight = lineHeight * maxRows + marginBlock;
+
+    if (height > maxHeight) {
+      ref.current.style.height = maxHeight + "px";
+      ref.current.style.overflowY = "auto";
+      return;
+    }
+    ref.current.style.height = height + "px";
+    ref.current.style.overflowY = "hidden";
+  };
+
   function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
     if (state.current !== "enabled") return;
     setValue(e.target.value);
+    calcFitContentHeight();
   };
 
   if (hook) {
@@ -94,14 +122,13 @@ export function TextArea<D extends Schema.DataItem<Schema.$String>>({
 
   const rows = (() => {
     if (propsRows == null) return DEFAULT_ROWS;
-    if (propsRows === "fit") {
-      if (!value) return 1;
-      const len = Math.max(1, value.split(/\r?\n/).length);
-      if (maxRows == null) return len;
-      return Math.min(len, maxRows);
-    }
+    if (propsRows === "fit") return 1;
     return propsRows;
   })();
+
+  useEffect(() => {
+    calcFitContentHeight();
+  }, []);
 
   return (
     <InputField
