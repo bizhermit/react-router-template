318e3d3b25d74c31dbb4b347b5e359cbe5c9167d
bizhermit <rsenda@bizhermit.com>
2025-08-29 21:15:52 +0900

    fix: remove docekr prune

--- .devcontainer/removeDocker.sh
+++ .devcontainer/removeDocker.sh
@@ -27,7 +27,9 @@ echo "Cleanup completed."
 echo "docker prune start."
 
 docker container prune -f
+docker network prune -f
 docker volume prune -f
-docker image prune -f
+docker image prune -a -f
+docker builder prune -a -f
 
 echo "docker prune completed."
