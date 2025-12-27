c2efdab538c38129914001a1a476b9023ab67360
bizhermit <rsenda@bizhermit.com>
2025-12-27 23:12:45 +0900

    fix: env

--- src/app/routes/health.ts
+++ src/app/routes/health.ts
@@ -6,7 +6,7 @@ type Get = Api.SuccessResponse<InternalApiPaths, "/health", "get">["data"];
 
 export async function loader() {
   // eslint-disable-next-line no-console
-  console.log("healthcheck:", process.version);
+  console.log(`healthcheck: ${process.version} / ${process.env.NODE_ENV}`);
   return data({
     now: DateTime.now("UTC").toString(),
   } satisfies Get);
