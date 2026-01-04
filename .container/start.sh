cb1cd6b5a7805699b124f57497b747ca244bc91c
bizhermit <rsenda@bizhermit.com>
2026-01-04 14:34:52 +0900

    fix: docker

--- .container/start.sh
+++ .container/start.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-./wait-for-it.sh "$WAITFOR" -t 60
+if [ -n "${WAITFOR:-}" ]; then
+  echo "[init] Waiting for DB..."
+  ./wait-for-it.sh "$WAITFOR" -t 60
+fi
 
-echo "Running migrations..."
-npm run migrate
+if [ "${MIGRATE:-true}" = "true" ]; then
+  echo "[init] Running migrations..."
+  npm run migrate
+fi
 
-echo "Starting app..."
-npm run start
+echo "[app] Starting app..."
+exec npm run start
