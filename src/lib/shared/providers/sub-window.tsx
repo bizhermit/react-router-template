d30501bd88e4e227480a6522041004c9543dfffc
bizhermit <rsenda@bizhermit.com>
2026-02-11 17:58:06 +0900

    feat: sub window controller

---
+++ src/lib/shared/providers/sub-window.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, type ReactNode } from "react";
+import { SubWindowContext, type SubWindowCloseTrigger, type SubWindowControllerContext } from "../hooks/sub-window";
+
+const DEFAULT_CLOSE_TRIGGER: SubWindowCloseTrigger = {};
+
+export function SubWindowProvider(props: {
+  closeTrigger?: SubWindowCloseTrigger;
+  children?: ReactNode;
+}) {
+  const wins = useRef<SubWindowControllerContext[]>([]);
+
+  function append(ctx: SubWindowControllerContext) {
+    wins.current.push(ctx);
+  };
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    function beforeUnloadEvent() {
+      for (let i = wins.current.length - 1; i >= 0; i--) {
+        const { controller, closeTrigger } = wins.current[i];
+        if (
+          closeTrigger.transitionPage ||
+          closeTrigger.closeTab
+        ) {
+          controller.close();
+          wins.current.splice(i, 1);
+        }
+      }
+    };
+    window.addEventListener("beforeunload", beforeUnloadEvent);
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnloadEvent);
+    };
+  }, []);
+
+  return (
+    <SubWindowContext
+      value={{
+        append,
+        closeTrigger: props.closeTrigger ?? DEFAULT_CLOSE_TRIGGER,
+      }}
+    >
+      {props.children}
+    </SubWindowContext>
+  );
+};
