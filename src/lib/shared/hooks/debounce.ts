0272565fdd73a72afb418cd5b25d2aff09913378
bizhermit <rsenda@bizhermit.com>
2026-02-18 09:42:51 +0900

    feat: debounce leading

--- src/lib/shared/hooks/debounce.ts
+++ src/lib/shared/hooks/debounce.ts
@@ -19,21 +19,71 @@ export function useDebounce<T extends Array<unknown>>(
     }, delay);
   }, [delay]);
 
-  function cancel() {
+  const cancel = useCallback(() => {
     if (t.current) {
       clearTimeout(t.current);
       t.current = null;
     }
-  };
+  }, []);
 
   useEffect(() => {
     return () => {
       cancel();
     };
-  }, []);
+  }, [cancel]);
 
   return [
     debounceCallback,
+    cancel,
+  ] as const;
+};
+
+export function useDebounceLeading<T extends Array<unknown>>(
+  callback: (...args: T) => void,
+  delay = 0,
+  noExtend = false
+) {
+  const t = useRef<NodeJS.Timeout | null>(null);
+  const c = useRef(callback);
+  useLayoutEffect(() => {
+    c.current = callback;
+  }, [callback]);
+
+  const debounceLeadingCallback = useCallback((...args: T) => {
+    if (t.current) {
+      if (noExtend) return;
+      clearTimeout(t.current);
+    }
+
+    const shouldInvoke = !t.current;
+
+    t.current = setTimeout(() => {
+      t.current = null;
+    }, delay);
+
+    if (shouldInvoke) {
+      c.current(...args);
+    }
+  }, [
+    delay,
+    noExtend,
+  ]);
+
+  const cancel = useCallback(() => {
+    if (t.current) {
+      clearTimeout(t.current);
+      t.current = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      cancel();
+    };
+  }, [cancel]);
+
+  return [
+    debounceLeadingCallback,
     cancel,
   ] as const;
 };
--- src/lib/shared/timing/debounce.ts
+++ src/lib/shared/timing/debounce.ts
@@ -33,3 +33,44 @@ export default function debounce<T extends Array<unknown>>(
 
   return f;
 };
+
+/**
+ * 連続する最初の処理を実行し、その後はロックが解除されるまで呼び出しを無視する
+ * @param callback
+ * @param delay ロックを解除する間隔
+ * @param noExtend ロック期間延長フラグ
+ * - false (default): 連打されるとロック期間が延長される
+ * - true: 連打されてもロック期間は延長されない
+ * @returns
+ */
+export function debounceLeading<T extends Array<unknown>>(
+  callback: (...args: T) => void,
+  delay = 0,
+  noExtend = false
+) {
+  let t: NodeJS.Timeout | null = null;
+
+  const f = function (this: unknown, ...args: T) {
+    if (t) {
+      if (noExtend) return;
+      clearTimeout(t);
+    }
+
+    const shouldInvoke = !t;
+
+    t = setTimeout(() => {
+      t = null;
+    }, delay);
+
+    if (shouldInvoke) callback.apply(this, args);
+  } as DebounceFunc<T>;
+
+  f.cancel = function () {
+    if (t) {
+      clearTimeout(t);
+      t = null;
+    }
+  };
+
+  return f;
+};
