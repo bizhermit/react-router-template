392826b343e1914e10722caca30cccdbe7db7d66
bizhermit <rsenda@bizhermit.com>
2026-03-27 09:15:33 +0900

    fix: e2e test doc

--- playwright/screenshot.spec.ts
+++ playwright/screenshot.spec.ts
@@ -1,7 +1,7 @@
 import test from "@playwright/test";
 import { screenShot } from "./utilities";
 
-test("screenshots", async ({ page }) => {
+test("document-injection-screenshots", async ({ page }) => {
   await page.goto("/");
   await screenShot(page, "index");
   await page.goto("/sandbox");
